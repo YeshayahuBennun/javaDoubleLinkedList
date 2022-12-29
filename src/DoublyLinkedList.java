e735396a1f95d869aa6e205e90138b1916d78a81
YeshayahuBennun <ybennun@yahoo.com>
2022-12-29 19:01:02 +0200

    Commit#1 - Creation

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,33 @@
+public class DoublyLinkedList {
+    private DoublyNode head;
+    private DoublyNode tail;
+    private int size;
+
+    public DoublyLinkedList() {
+        //Empty
+    }
+
+    public DoublyNode getHead() {
+        return head;
+    }
+
+    public void setHead(DoublyNode head) {
+        this.head = head;
+    }
+
+    public DoublyNode getTail() {
+        return tail;
+    }
+
+    public void setTail(DoublyNode tail) {
+        this.tail = tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
---
+++ src/DoublyNode.java
@@ -0,0 +1,33 @@
+public class DoublyNode {
+    private int value;
+    private DoublyNode next;
+    private DoublyNode prev;
+
+    public DoublyNode() {
+        //Empty
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public DoublyNode getNext() {
+        return next;
+    }
+
+    public void setNext(DoublyNode next) {
+        this.next = next;
+    }
+
+    public DoublyNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DoublyNode prev) {
+        this.prev = prev;
+    }
+}
