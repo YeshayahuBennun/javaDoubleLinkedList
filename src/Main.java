5256b2605b54127acd17b5a5e2b1a0a93e0f9745
YeshayahuBennun <ybennun@yahoo.com>
2023-01-03 19:25:17 +0200

    Commit#7 - Deletion Method in Doubly Linked List

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
         DoublyLinkedList dLL = new DoublyLinkedList();
 
         dLL.createDLL(1);
-        dLL.insertDLL(2,0);
-        dLL.insertDLL(3,1);
-        dLL.insertDLL(4,7);
+        dLL.insertDLL(2, 0);
+        dLL.insertDLL(3, 1);
+        dLL.insertDLL(4, 7);
         dLL.traversalDLL();
-        dLL.reverseTraversalDLL();
-        dLL.searchDLL(7);
+        dLL.deletionDLL(7);
+        dLL.traversalDLL();
     }
 }
